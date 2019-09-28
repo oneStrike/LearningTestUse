a71c3479ebf936dd507e3263c7c031318fb3e0ff
AIURA <759994689@qq.com>
2019-09-28 17:24:14 +0800

    121321

--- js/public.js
+++ js/public.js
@@ -1,6 +1,30 @@
-function test(){
-    var a =10;
-    var b=20;
-    return a+b;
-}
-console.log(test())
+//格式化时间字符串
+String.prototype.myFormatTime = function myFormatTime(template = '{0}年{1}月{2}日 {3}时{4}分{5}秒') {
+    let ary = this.match(/\d+/g).map(item => (item < 10 ? '0' + item : item));
+    return template.replace(/\{(\d)\}/g, (...[, index]) => ary[index] || '00');
+};
+
+//获取一个字符串中最多出现字符的次数和对应的字符
+let myPublic = (function () {
+    return {
+        sortStr: function (str) {
+            var obj = {};
+            for (var i = 0; i < str.length; i++) {
+                var element = str[i];
+                obj[element] ? obj[element]++ : obj[element] = 1;
+            };
+            var result = {
+                frequency: 0,
+                str: ''
+            };
+            for (var i in obj) {
+                if (obj[i] > result['frequency']) {
+                    result['frequency'] = obj[i];
+                    result['str'] = i;
+                }
+            }
+            return result;
+        },
+
+    }
+}());
