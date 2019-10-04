4838ec75c274cf9964bbdc6c662b72ef60e1706b
AIURA <759994689@qq.com>
2019-10-04 17:17:22 +0800

    2

--- js/Distribution.js
+++ js/Distribution.js
@@ -1,11 +1,12 @@
 $(function () {
-    let imgData = null,
-        page = 0;
-    //获取数据
-    let queryData = function () {
+    let page = 0,
+        imgData = null,
+        $liList = $('.flowBox li'),
+        isRun = false;
+    let getData = function () {
         page++;
         $.ajax({
-            url: '../json/data.json?page=${page}',
+            url: `../json/data.json?page=${page}`,
             method: 'GET',
             async: false,
             dataType: 'json',
@@ -14,48 +15,43 @@ $(function () {
             }
         });
     };
-    queryData();
+    getData();
 
-    //绑定数据
-
-    //=>获取到ul下的三个li
-    let $listBox = $('.flowBox>li').get();
-
-    //=>创建li内需要展示的元素
-    let queryHTML = function (obj = {}) {
-
-        let {
-            id,
-            title,
-            link,
-            pic
-        } = obj;
-        if (typeof (id) === 'undefined') {
-            return '';
-        }
-        return `<a href="${link}">
-                     <div><img src="${pic}" alt=""></div>
-                     <span>${title}</span>
-                 </a>`;
+    let setData = function () {
+        for (var i = 0; i < imgData.length; i += 3) {
+            $liList.sort(function (a, b) {
+                return $(a).outerHeight() - $(b).outerHeight();
+            }).each(function (index, item) {
+                if (!(imgData[i + index])) return;
+                let {
+                    title,
+                    pic,
+                    link,
+                    id
+                } = imgData[i + index];
+                $(`<a href="${link}">
+                    <div><img src="${pic}" alt=""></div>
+                    <span>${title}</span>
+                </a>`).appendTo($(item));
+            })
+        };
+        isRun = false;
     };
+    setData();
 
-    //=>循环json数据，获取需要展示的图片
-    for (let i = 0; i < imgData.length; i += 3) {
-        //=>将获取到的数据分组，三个一组
-        let item1 = imgData[i],
-            item2 = imgData[i + 1],
-            item3 = imgData[i + 2];
-
-        //=>对比每个li的高度，依次将获取到的元素插入
-        $listBox.sort(function (a, b) {
-            return a.offsetHeight - b.offsetHeight;
-        });
-
-        //=>插入创建的元素
-        $listBox.forEach(function (item, index) {
-            item.innerHTML += queryHTML(eval('item' + (index + 1)))
-        })
-
-    }
-
+    $(window).on('scroll', function () {
+        let $winH = $(window).outerHeight(),
+            pageH = document.documentElement.scrollHeight || document.body.scrollHeight,
+            $scrollT = $(window).scrollTop();
+        if (($scrollT + 100) >= (pageH - $winH)) {
+            if (isRun) return;
+            isRun = true;
+            if (page > 5) {
+                alert('没有更多数据了');
+                return;
+            }
+            getData();
+            setData();
+        }
+    })
 })
--- js/mallSort.js
+++ js/mallSort.js
@@ -1,81 +1,71 @@
 $(function () {
-    let productRender = (function () {
-        let productData = null,
-            listBox = document.getElementById('list'),
-            headerBox = document.getElementById('header'),
-            linkList = headerBox.getElementsByTagName('a'),
-            liList = listBox.getElementsByTagName('li');
-        let getData = function () {
-            $.ajax({
-                url: '../json/mallSort.json',
-                method: 'GET',
-                async: false,
-                dataType: 'json',
-                success: function (result) {
-                    productData = result;
-                }
-            })
-        };
-
-        let setELe = function () {
-            let str = ``;
-            for (var i = 0; i < productData.length; i++) {
+    let productData = (function () {
+        let showData = null,
+            $showBox = $('.list'),
+            $clickTag = $('.header > a');
+        //=>获取数据
+        $.ajax({
+            url: '../json/mallSOrt.json',
+            method: 'GET',
+            async: false,
+            dataType: 'json',
+            success: function (result) {
+                showData = result;
+            }
+        });
+        //绑定数据;
+        let bindData = function () {
+            for (var i = 0; i < showData.length; i++) {
                 let {
-                    time,
-                    price,
+                    title,
                     hot,
+                    price,
                     img,
-                    title
-                } = productData[i];
-                str += `<li data-time="${time}" data-price="${price}" data-hot="${hot}">
-                    <a href="javascript:;">
+                    time
+                } = showData[i];
+                $(`<li data-time="${time}" data-price="${price}" data-hot="${hot}">
+                <a href="javascript:;">
                     <img src="${img}" alt="">
                     <p>${title}</p>
-                    <span>￥${price}</span><br>
-                    <span>上架日期:${time}</span><br>
-                    <span>热度：${hot}</span>
-                </a></li>`;
-            };
-            listBox.innerHTML = str;
+                    <span>￥${price}</span>
+                    <span>日期：￥${time}</span>
+                    <span>热度${hot}</span>
+                </a>
+            </li>`).appendTo($showBox);
+            }
         };
-
-        let clickEve = function () {
-            liList = Array.from(liList);
+        //绑定点击事件
+        let bindClick = function () {
+            let $liList = $('.list li');
             let arr = ['data-time', 'data-price', 'data-hot'];
-            for (let i = 0; i < linkList.length; i++) {
-                linkList[i].flag = -1;
-                linkList[i].onclick = function () {
-                    for (var k = 0; k < linkList.length; k++) {
-                        linkList[k].flag = -1;
+            $clickTag.each(function (index, item) {
+                item.flag = -1;
+                $(item).click(function () {
+                    for (var i = 0; i < $clickTag.length; i++) {
+                        if (this !== $clickTag[i]) {
+                            $clickTag[i].flag = -1;
+                        }
                     }
-                    linkList[i].flag *= -1;
-                    liList.sort(function (a, b) {
-                        a = a.getAttribute(arr[i]);
-                        b = b.getAttribute(arr[i]);
-                        if (i === 0) {
-                            a = a.replace(/-/g, '');
-                            b = b.replace(/-/g, '');
+                    item.flag *= -1;
+                    $liList.sort(function (firstItem, lastItem) {
+                        firstItem = firstItem.getAttribute(arr[index]);
+                        lastItem = lastItem.getAttribute(arr[index]);
+                        if (index == 0) {
+                            firstItem = firstItem.replace(/-/g, '');
+                            lastItem = lastItem.replace(/-/g, '');
                         };
-                        return (a - b) * linkList[i].flag;
-                    });
-                    let frag = document.createDocumentFragment();
-                    for (var j = 0; j < liList.length; j++) {
-                        frag.appendChild(liList[j])
-                    };
-                    listBox.appendChild(frag);
-                    frag = null;
-                }
-            };
-
-        };
-
+                        return (firstItem - lastItem) * item.flag;
+                    }).appendTo($showBox);
+                });
+            });
+        }
+        //高级单例模式，返回函数
         return {
             init: function () {
-                getData();
-                setELe();
-                clickEve();
+                bindData();
+                bindClick();
             }
         }
-    }());
-    productRender.init();
+    })();
+    productData.init();
 })
