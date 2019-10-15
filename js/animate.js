0ddde020648e56809918430d08e8675cf3ac9adb
AIURA <759994689@qq.com>
2019-10-15 17:29:50 +0800

    1

--- js/animate.js
+++ js/animate.js
@@ -1,80 +1,101 @@
-(function () {
-    window.utils = (function () {
-        let getCss = function (ele, sty) {
+/*==ANIMATE动画库==*/ ~ function () {
+    //=>准备操作CSS样式的方法 GET-CSS/SET-CSS/SET-GROUP-CSS/CSS
+    let utils = (function () {
+        //=>获取样式
+        let getCss = (ele, attr) => {
             let val = null,
-                reg = /^-?\d+(\.\d+)?(px|rem|em|pt)?$/g;
+                reg = /^-?\d+(\.\d+)?(px|rem|em)?$/;
             if ('getComputedStyle' in window) {
-                val = window.getComputedStyle(ele)[sty];
-            } else {
-                val = ele['style'][sty];
+                val = window.getComputedStyle(ele)[attr];
+                if (reg.test(val)) {
+                    val = parseFloat(val);
+                }
             }
-            reg.test(val) ? val = window.parseFloat(val) : null;
             return val;
         };
-        let setCss = function (ele, sty, val) {
-            let reg = /^(zIndex|opacity)$/g;
-            if (!reg.test(sty) && !isNaN(val)) {
-                val += 'px';
+
+        //=>设置样式
+        let setCss = (ele, attr, value) => {
+            if (!isNaN(value)) {
+                if (!/^(opacity|zIndex)$/.test(attr)) {
+                    value += 'px';
+                }
             }
-            ele['style'][sty] = val;
+            ele['style'][attr] = value;
         };
-        let setGroupCss = function (ele, obj) {
-            for (let key in obj) {
-                if (obj.hasOwnProperty(key)) {
-                    setCss(ele, key, obj[key])
+
+        //=>批量设置样式
+        let setGroupCss = (ele, options) => {
+            for (let attr in options) {
+                if (options.hasOwnProperty(attr)) {
+                    setCss(ele, attr, options[attr]);
                 }
             }
         };
-        let css = function (...arr) {
-            let fn = getCss,
-                len = arr.length;
-            if (len === 2 && typeof arr[1] === 'object') {
-                fn = setGroupCss;
-            } else if (len === 3) {
+
+        //=>合并为一个
+        let css = (...arg) => {
+            let len = arg.length,
+                fn = getCss;
+            if (len >= 3) {
                 fn = setCss;
             }
-            return fn(...arr)
-        }
+            if (len === 2 && typeof arg[1] === 'object') {
+                fn = setGroupCss;
+            }
+            return fn(...arg);
+        };
+
         return {
             css
         }
-    }());
-    let formula = {
-        linear: function (t, d, c, b) {
-            return t / d * c + b;
-        }
+    })();
+
+    //=>EFFECT：准备运动的公式
+    let effect = {
+        Linear: (t, b, c, d) => t / d * c + b
     };
+
+    //=>封装动画库
     window.animate = function (ele, target = {}, duration = 1000, callback = new Function()) {
-        if (typeof (duration) === 'function') {
+        //=>不传递CALL-BACK，让其默认为一个空函数（回调函数：当动画结束后做什么事，都放到回调函数完成即可）
+        if (typeof duration === 'function') {
+            //=>我们有四个形参，但是传递的时候只传递三个，最后一个回调函数传递给duration这个参数了，我们需要改一下参数的值
             callback = duration;
             duration = 1000;
         }
-        let time = 0,
+
+        //1.基于TARGET计算出BEGIN/CHANGE
+        let begin = {},
             change = {},
-            begin = {};
-        for (let key in target) {
-            if (target.hasOwnProperty(key)) {
-                begin[key] = utils.css(ele, key);
-                change[key] = target[key] - begin[key];
+            time = 0;
+        for (let attr in target) {
+            if (target.hasOwnProperty(attr)) {
+                begin[attr] = utils.css(ele, attr);
+                change[attr] = target[attr] - begin[attr];
             }
         }
-        clearInterval(ele.timer)
 
-        let cur = {};
-        ele.timer = setInterval(function () {
+        //2.实现动画
+        clearInterval(ele.animateTimer); //=>在给当前元素设置新的动画之前,先清空原有正在运行的动画(防止多动画共存,把动画的返回值赋值给当前元素的自定义属性，这样只要元素不变，我们不管啥时候在哪执行都可以清除元素的动画)
+        ele.animateTimer = setInterval(() => {
             time += 17;
+            //=>边界判断
             if (time >= duration) {
-                clearInterval(ele.timer);
                 utils.css(ele, target);
-                callback();
+                clearInterval(ele.animateTimer);
+
+                callback.call(ele); //=>动画完成后执行CALL-BACK(并且让回调函数中的THIS是当前操作的元素本身)
                 return;
             }
-            for (let key in target) {
-                if (target.hasOwnProperty(key)) {
-                    cur[key] = formula.linear(time, duration, change[key], begin[key]);
+            //=>依托TARGET计算出每个方向的当前位置
+            let cur = {};
+            for (let attr in target) {
+                if (target.hasOwnProperty(attr)) {
+                    cur[attr] = effect.Linear(time, begin[attr], change[attr], duration);
                 }
             }
             utils.css(ele, cur);
-        }, 17)
-    }
-}());
+        }, 17);
+    };
+}();
