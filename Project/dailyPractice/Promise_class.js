10172f222a05e269a62367f7115bf2e02883fd20
oneStrike <759994689@qq.com>
2020-05-04 10:42:39 +0800

    04

---
+++ Project/dailyPractice/Promise_class.js
@@ -0,0 +1,132 @@
+(function () {
+  const PENDING = "pending";
+  const RESOLVED = "resolved";
+  const REJECTED = "rejected";
+  class Promise {
+    constructor(executor) {
+      const self = this;
+      self.status = PENDING;
+      self.data = null;
+      self.callbacks = [];
+
+      function resolve(value) {
+        if (self.status !== PENDING) return;
+        self.status = RESOLVED;
+        self.data = value;
+        setTimeout(function () {
+          self.callbacks.forEach(function (callback) {
+            callback.onReseolved(self.data);
+          });
+        });
+      }
+
+      function reject(reason) {
+        if (self.status !== PNEDING) return;
+        self.status = REJECTED;
+        self.data = reason;
+        self.callbacks.forEach(function (callback) {
+          callback.onRejected(self.data);
+        });
+      }
+      executor(resolve, reject);
+    }
+
+    then(onReseolved, onRejected) {
+      const self = this;
+      onResolved =
+        typeof onResolved === "function" ? onResolved : (value) => value;
+      onRejected =
+        typeof onRejected === "function"
+          ? onRejected
+          : (reason) => {
+              throw reason;
+            };
+      return new Promise(function (resolve, reject) {
+        function handle(callback) {
+          try {
+            let result = callback(self.data);
+            if (result instanceof Promise) {
+              result.then(resolve, reject);
+            } else {
+              resolve(result);
+            }
+          } catch (error) {
+            reject(error);
+          }
+        }
+
+        if (self.status === RESOLVED) {
+          handle(onReseolved);
+        } else if (self.status === REJECTED) {
+          handle(onRejected);
+        } else
+          self.callbacks.push({
+            onResolved: function () {
+              handle(onResolved);
+            },
+            onRejected: function () {
+              handel(onRejected);
+            },
+          });
+      });
+    }
+
+    catch(onRejected) {
+      return new Promise(function (resolve, reject) {
+        this.then(undefined, onRejected);
+      });
+    }
+
+    static resolve(value) {
+      return new Promise(function (resolve, reject) {
+        if (value instanceof Promise) {
+          value.then(
+            (val) => resolve(val),
+            (reason) => reject(reason)
+          );
+        } else {
+          resolve(value);
+        }
+      });
+    }
+
+    static reject(reason) {
+      return new Promise(function (resolve, reject) {
+        reject(reason);
+      });
+    }
+
+    static all(promiseArr) {
+      return new Promise(function (resolve, reject) {
+        let values = [];
+        let count = 0;
+        promiseArr.forEach(function (item, index) {
+          item.then(
+            (value) => {
+              count++;
+              values[index] = value;
+              if (count === promiseArr.length) {
+                resolve(values);
+              }
+            },
+            (reason) => {
+              reject(reason);
+            }
+          );
+        });
+      });
+    }
+
+    static race(promiesArr) {
+      return new Promise(function (resolve, reject) {
+        promiseArr.forEach(function (item) {
+          item.then(
+            (value) => resolve(value),
+            (reason) => reject(reason)
+          );
+        });
+      });
+    }
+  }
+  window.Promise = Promise;
+})();
--- Project/dailyPractice/test.js
+++ Project/dailyPractice/test.js
@@ -1,3 +1,12 @@
-let dtr = '465464';
-console.log(dtr.charAt(0));
-console.log(dtr.length)
+async function t() {
+  try {
+    let [result, result2] = await Promise.all([
+      Promise.resolve(1),
+      Promise.resolve(2),
+    ]);
+    return [result, result2];
+  } catch (err) {
+    //=>捕获错误
+  }
+}
+t().then((value) => console.log(value));
